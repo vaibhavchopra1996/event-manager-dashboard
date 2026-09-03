81b26f69c9be1d030941153149716bcad5f67732
Vaibhav Chopra <vaibhav.chopra@ebizondigital.com>
2026-09-03 13:22:18 +0530

    Update API URL for deployment

--- frontend/app/utils/api.ts
+++ frontend/app/utils/api.ts
@@ -1,4 +1,6 @@
-const BASE_URL = 'http://localhost:5000/api';
+// const BASE_URL = 'http://localhost:5000/api';
+
+const BASE_URL = 'https://onrender.com';
 
 export interface Event {
   id?: number;
